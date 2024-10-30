3e530cb3d85cdba480b32c42cfadc7f73b72deb0
xkenn <ervin.gonzales.inno@gmail.com>
2024-10-30 23:52:29 +0800

    feat: integrate login form

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,9 +1,17 @@
+import { LoginFormComponent } from "@/components/login-form";
 import React from "react";
 
 const SignIn = () => {
   return (
-    <div>
-      <h1>Sign In</h1>
+    <div className="h-screen w-full flex flex-row">
+      <div className="w-[40%] bg-gray-400 rounded-lg flex items-center justify-center ">
+        <div className="flex-row w-full items-center justify-center px-10">
+          <LoginFormComponent />
+        </div>
+      </div>
+      <div className="w-auto">
+        <h1>Sign In</h1>
+      </div>
     </div>
   );
 };
